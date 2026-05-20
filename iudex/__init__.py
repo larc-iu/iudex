d9c809fc35f337626d0d7c509bd955ab2cb1afbc
Luke Gessler <lukegessler@gmail.com>
2026-05-20 13:44:30 -0400

    a4

--- iudex/__init__.py
+++ iudex/__init__.py
@@ -13,7 +13,7 @@ To add a sibling framework, append its dotted path to `FRAMEWORKS` and
 give its `__init__.py` the three required attributes.
 """
 
-__version__ = "0.1.0a3"
+__version__ = "0.1.0a4"
 
 FRAMEWORKS: list[str] = ["iudex.rst"]
 
